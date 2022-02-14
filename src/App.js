aa8650b1dd45b0f63c0f407ce8a0d1e5ed187827
abhishekbhartiind <imabhishekbharti12@gmail.com>
2022-02-14 23:15:05 +0530

    working with web3.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/UI/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="main-header">
+        <h1>Zen &amp; Web3</h1>
+        <nav className="nav">
+          <ul>
+            <li>Wallet Address</li>
+            <li>
+              0x145420D3a0d2D410A690E47fe26F19B76e20E272
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <Card className="login">
+          <button className="button" type="button">
+            Connect
+          </button>
+        </Card>
+        <Card className="home">
+          <h1>Welcome</h1>
+        </Card>
+      </main>
     </div>
   );
 }
